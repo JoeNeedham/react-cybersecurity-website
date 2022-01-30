440b833e37ff7058c8922c56a8d9fc932bbe628e
joeneedum <joeneedum@gmail.com>
2022-01-30 18:51:34 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './globalStyles'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Test from './test';
+
+//Pages
+import Home from './pages/Home';
+import SignUp from './pages/SignupPage';
+import Pricing from './pages/PricingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <GlobalStyle />
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route exact path='/signup' element={<SignUp/>} />
+            <Route exact path='/pricing' element={<Pricing/>} />
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MainHeading } from './globalStyles';
+
+const test = () => {
+    return (
+    <MainHeading>HELLO WORLD</MainHeading>
+    ) 
+};
+
+export default test;
