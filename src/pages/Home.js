4546cad1941f33dd48e4285db2ab58f5727941e0
joeneedum <joeneedum@gmail.com>
2022-02-01 20:28:43 -0500

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Content } from '../components/Content/Content';
 import Hero from '../components/Hero/Hero';
 import Features from '../components/Features/Features';
+import { heroOne, heroTwo, heroThree } from '../data/HeroData';
 
 const Home = () => {
     return (
     <>
-    <Hero />
-    <Features />
+        <Hero />
+        <Features />
+        <Content {...heroOne} />
+        <Content {...heroTwo} />
+        <Content {...heroThree} />
     </>
     )
 
